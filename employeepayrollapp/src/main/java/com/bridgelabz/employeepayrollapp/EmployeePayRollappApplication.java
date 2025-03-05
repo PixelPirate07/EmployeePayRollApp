e42fd8396cdcc85c3f17ee023888f699e74b2c5c
PixelPirate07 <pandeydhairya02@gmail.com>
2025-03-05 21:38:04 +0530

    UC1 with Project Creation added

---
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollappApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayRollappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayRollappApplication.class, args);
+	}
+
+}
