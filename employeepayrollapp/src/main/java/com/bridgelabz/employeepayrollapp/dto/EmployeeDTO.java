097ff93529becadf45f0e9d5d510d976d722265d
PixelPirate07 <pandeydhairya02@gmail.com>
2025-03-06 12:01:25 +0530

    UC4 with Services layer added

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -2,7 +2,9 @@ package com.bridgelabz.employeepayrollapp.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+@Data
 public class EmployeeDTO {
     @NotBlank(message="name cannot be empty")
     private String name;
