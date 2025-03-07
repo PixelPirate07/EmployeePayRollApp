d73aba1c8606bbcf015266cf05d9cb23d62ce353
PixelPirate07 <pandeydhairya02@gmail.com>
2025-03-07 14:59:12 +0530

    UC10 with Validation to Name Field added

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -2,10 +2,12 @@ package com.bridgelabz.employeepayrollapp.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class EmployeeDTO {
+    @Pattern(regexp = "^[A-Z][a-zA-Z]*$", message="Name must start with Capital Letter")
     @NotBlank(message="name cannot be empty")
     private String name;
     @NotNull(message="salary cannot be empty")
