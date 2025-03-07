398ddd135685196902792018a0b970ad6d9d5589
PixelPirate07 <pandeydhairya02@gmail.com>
2025-03-07 14:03:52 +0530

    UC5 with Interface and Exceptions added

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeService.java
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.employeepayrollapp.service;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
+import com.bridgelabz.employeepayrollapp.exception.EmployeeNotFoundException;
 import com.bridgelabz.employeepayrollapp.model.Employee;
 import com.bridgelabz.employeepayrollapp.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class EmployeeService {
+public class EmployeeService implements IEmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -28,12 +29,18 @@ public class EmployeeService {
     }
 
     public Employee updateEmployee(int id, EmployeeDTO employeeDTO) {
-        Employee employee=new Employee(employeeDTO);
-        employee.setId(id);
-        return employeeRepository.save(employee);
+        Employee existingEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee with ID " + id + " not found"));
+
+        existingEmployee.setName(employeeDTO.getName());
+        existingEmployee.setSalary(employeeDTO.getSalary());
+        return employeeRepository.save(existingEmployee);
     }
 
     public void deleteEmployee(int id) {
-        employeeRepository.deleteById(id);
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee with ID " + id + " not found"));
+        employeeRepository.delete(employee);
+
     }
 }
